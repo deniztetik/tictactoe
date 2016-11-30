f6f1d6999e646b06d44db44f2f591e90a786b917
Deniz Tetik <deniztetik31@gmail.com>
2016-11-30 15:33:30 -0500

    [feat] Finish basic game

--- script.js
+++ script.js
@@ -7,27 +7,67 @@ $(function() {
     $('button').click(function(event) {
       // change set buttons value and change turn after checking if the
       // space is already filled, and then check if a winning play has been played
+      // , and change text headers appropriately
       var button = $(event.target)
       if (!button.html()) {
         button.html(turn)
-        checkWinner(turn);
-        turn = (turn === "X" ? "O" : "X")
+        if (checkWinner(turn)) {
+          console.log("<h1>" + turn + " wins!!</h1>")
+          $("body").prepend("<h1>" + turn + " wins!!</h1>")
+        } else {
+          turn = (turn === "X" ? "O" : "X")
+          $("#turn").html(turn + "'s Turn")
+        }
       }
     })
   }
 
-  // function checkWinner(piece) {
-  //   // check rows
-  //   for (var i = 0; i < 3; i++) {
-  //     var piecesInRow = 0;
-  //     for (var j = 0; j < 3; j++) {
-  //       var className = "#" + i + "," + j
-  //       if ($(className).html() === piece) {
-  //
-  //       }
-  //     }
-  //   }
-  //
-  // }
+  function checkWinner(piece) {
+    // check rows
+    for (var i = 0; i < 3; i++) {
+      var piecesInRow = 0;
+      for (var j = 0; j < 3; j++) {
+        var idName = "#" + i + j
+        if ($(idName).html() === piece) {
+          piecesInRow++
+        }
+      }
+      if (piecesInRow === 3) return true
+    }
+
+    // check columns
+    for (var i = 0; i < 3; i++) {
+      var piecesInColumn = 0
+      for (var j = 0; j < 3; j++) {
+        var className = "#" + j + i
+        if ($(className).html() === piece) {
+          piecesInColumn++
+        }
+      }
+      if (piecesInColumn === 3) return true
+    }
+
+    // check first diagonal
+    var piecesInDiagonal = 0;
+    for (var i = 0; i < 3; i++) {
+      var className = "#" + i + i
+      if ($(className).html() === piece) {
+        piecesInDiagonal++
+      }
+    }
+    if (piecesInDiagonal === 3) return true
+
+
+    // check second diagonal
+    var piecesInDiagonal = 0;
+    for (var i = 0; i < 3; i++) {
+      var className = "#" + i + (2 - i)
+      if ($(className).html() === piece) {
+        piecesInDiagonal++
+      }
+    }
+    if (piecesInDiagonal === 3) return true
+    return false;
+  }
 
 })
